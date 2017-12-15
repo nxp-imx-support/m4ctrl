037447bbf3e009a52ce51312d5a3e057461eafa7
Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>
2017-12-15 11:03:55 +0200

    Add License text for all files
    Signed-off-by: Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>

--- include/m4ctrl.h
+++ include/m4ctrl.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 #ifndef __M4CTRL_H__
 #define __M4CTRL_H__		1
 
--- include/platform/imx_v6_v7.h
+++ include/platform/imx_v6_v7.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 #ifndef __IMX_V6_V7_H__
 #define __IMX_V6_V7_H__		1
 
